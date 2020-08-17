f691e190ea438ab517efbaae4d05cd7a2bd3327e
Rahul Gururaj <rahul.gururaj96@gmail.com>
2020-08-17 20:40:31 +1000

    Added activity-list and activity components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActivityListComponent } from './component/activity-list/activity-list.component';
+import { ActivityComponent } from './component/activity/activity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActivityListComponent,
+    ActivityComponent
   ],
   imports: [
     BrowserModule,
